a0bef091c45d11e4b310c182155ffd21c9cf131d
Ann Grace Evans <agevans789@gmail.com>
2026-07-31 16:07:00 -0400

    install dependencies

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routes import discover
 
-app = FastAPI("American History Helper")
+app = FastAPI(title="American History Helper")
 
 app.add_middleware(
     CORSMiddleware, 
